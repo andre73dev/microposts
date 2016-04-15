d010a66d247cac66761841b2c88600d634b0d269
andre73dev <andre73.dev@gmail.com>
2016-04-15 00:03:23 +0000

    add location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def show # 追加
     @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
@@ -16,10 +19,24 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Update Profile"
+      redirect_to @user
+    else
+      render user_path
+    end
+  end    
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :location)
   end
 
 end
